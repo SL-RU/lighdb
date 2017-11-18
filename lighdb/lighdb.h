c7da0d32f8213afbabd6e76c27c6e847dc2aa8f6
Alexander Lutsai <s.lyra@ya.ru>
2017-11-19 01:26:54 +0300

    Header done

--- lighdb/lighdb.h
+++ lighdb/lighdb.h
@@ -1,5 +1,11 @@
+/*
+  Author: Alexander Lutsai <s.lyra@ya.ru>
+  LICENSE: BSD 2-Clause License
+*/
 #ifndef LIGHDB_H
 #define LIGHDB_H
+
+#define LIGHDB_VERSION "001"
 
 #include <stdint.h>
 
@@ -11,21 +17,44 @@
 #define LDB_FILE int
 #endif
 
+#ifndef LDB_MUTEX //FILE type
+#define LDB_MUTEX 0
+#endif
+#if LDB_MUTEX
+#define LDB_MUTEX_CREATE(...)
+#define LDB_MUTEX_DELETE(...)
+#define LDB_MUTEX_REQUEST(...)
+#define LDB_MUTEX_RELEASE(...)
+#else
+#define LDB_MUTEX_CREATE(...)  ldb_mutex_create
+#define LDB_MUTEX_DELETE(...)  ldb_mutex_delete
+#define LDB_MUTEX_REQUEST(...) ldb_mutex_request_grant
+#define LDB_MUTEX_RELEASE(...) ldb_mutex_release_grant
+#endif
+
 #ifndef SEEK_SET     //if fcntl.h doesn't included
 #define SEEK_SET 0   // Seek relative to begining of file
 #define SEEK_CUR 1   // Seek relative to current file position
 #define SEEK_END 2   // Seek relative to end of file
 #endif
 
+#ifndef LDB_MIN_ID_BUFF
+#define LDB_MIN_ID_BUFF 256
+#endif
 
 typedef enum {
     LDB_OK = 0,
-    LDB_ERR
+    LDB_ERR,
+    LDB_ERR_IO,
+    LDB_ERR_HEADER,
+    LDB_ERR_NO_BUFFER,
+    LDB_OK_SMALL_BUFFER,
+    LDB_ERR_SMALL_BUFFER,
 } LDB_RES;
 
 
 //You need to implement IO functions in your project. You can find samples in the implementations folder.
-//Or you 
+//Or you can set LIGHDB_USE_STDIO
 /**
  * Open file
  *
@@ -72,19 +101,161 @@ LDB_RES ldb_io_lseek(LDB_FILE *file, uint32_t offset, int whence);
 LDB_RES ldb_io_close(LDB_FILE *file);
 
 
-typedef struct {
-LDB_FILE file_index;
-LDB_FILE file_table;
-uint32_t columns_num;
-uint32_t * columns_width;
-uint32_t index_offset;
+//Database consists from two files: one with item's data one by one, other with header, some values and table of ID's for each data item
+/*
+  Index file structure:
+  |LightDB version(10bytes)|header_size(4bytes)|item_size(4bytes)|count(4bytes)|header(header_size bytes)|table of id(count*4 bytes)|
 
+  Data file structure:
+  |item's data one by one(item_size * count bytes)|
+*/
+
+
+typedef struct {
+    uint8_t opened;
+    
+    LDB_FILE file_data;   //file with data
+    LDB_FILE file_index;   //file with index
+
+    struct {
+	char version[10];
+	uint32_t header_size; //size of header
+	uint32_t item_size;   //size of single item
+	uint32_t count;       //total count of items
+    } h;
+    
+    uint32_t index_offset; //offset of ID datain file_index
+    //buffers
+    uint8_t *buffer1;
+    uint8_t *buffer2;
+    uint8_t *buffer_id;
+    uint32_t buffer_id_size;
+    
+#if LDB_MUTEX
+    LDB_MUTEX_t mutex; //mutex if enabled
+#endif
 } LighDB;
 
 
 
-LDB_RES ldb_open();
+/**
+ * Open existing DB
+ *
+ * @param db pointer to DB structure
+ * @param path_index path to index file of DB
+ * @param path_data path to data file of DB
+ * @param buffer_size returns buffer size for DB functionality. You need to set buffer using ldb_set_buffer(); Size equals [(size of item) * 2 + N] bytes. N byte for buffer of indexes, more is better. But not less then 200 bytes
+ * @return result LDB_OK, LDB_ERR, LDB_ERR_IO
+ */
+LDB_RES ldb_open(LighDB *db,
+		 char *path_index, char *path_data,
+		 uint32_t* buffer_size);
 
-
+/**
+ * Close opened DB. After DB buffer doesn't required anymore.
+ *
+ * @param db pointer to DB structure
+ * @return result LDB_OK, LDB_ERR_IO
+ */
+LDB_RES ldb_close(LighDB *db);
+/**
+ * Set buffer. 
+ *
+ * @param db pointer to DB structure 
+ * @param buffer buffer
+ * @param size size of buffer
+ * @retur result LDB_OK, LDB_ERR_SMALL_BUFFER
+ */
+LDB_RES ldb_set_buffer(LighDB *db, uint8_t *buffer, uint32_t size);
+/**
+ * Create new database
+ *
+ * @param db pointer to DB structure
+ * @param path_data path to data DB file
+ * @param path_index path to index DB file
+ * @param size size of a single item's data
+ * @param header_size size of header
+ * @param header header buffer
+ * @param buffer_size returns buffer size for DB functionality. You need to set buffer using ldb_set_buffer(); Size equals [(size of data) * 2 + N] bytes. N byte for buffer of indexes, more is better. But not less then 200 bytes
+ * @return result LDB_OK, LDB_ERR_IO
+ */
+LDB_RES ldb_create(LighDB *db, char *path_data, char *path_index,
+		   uint32_t size,
+		   uint32_t header_size, uint8_t *header,
+		   uint32_t* buffer_size);
+/**
+ * Get data from first found item by ID
+ *
+ * @param db pointer to DB structure
+ * @param id ID of the data 
+ * @param buf buffer of data
+ * @param size size of data
+ * @return result LDB_OK, LDB_ERR_IO, LDB_ERR_SMALL_BUFFER
+ */
+LDB_RES ldb_get(LighDB *db, uint32_t id,
+		uint8_t *buf, uint32_t size);
+/**
+ * Change data on first found item by ID
+ *
+ * @param db pointer to DB structure
+ * @param id ID of the data 
+ * @param buf buffer of data
+ * @param size size of data
+ * @return result LDB_OK, LDB_ERR_IO, LDB_ERR_SMALL_BUFFER
+ */
+LDB_RES ldb_upd(LighDB *db, uint32_t id,
+		void *data, uint32_t size);
+/**
+ * Get data from item by index
+ *
+ * @param db pointer to DB structure
+ * @param index index of item
+ * @param buf buffer of data
+ * @param size size of data
+ * @return result LDB_OK, LDB_ERR_IO, LDB_ERR_SMALL_BUFFER
+ */
+LDB_RES ldb_get_ind(LighDB *db, uint32_t index,
+		    uint8_t *buf, uint32_t size);
+/**
+ * Change item's data by index
+ *
+ * @param db pointer to DB structure
+ * @param index index of row 
+ * @param buf buffer of data
+ * @param size size of data
+ * @return result LDB_OK, LDB_ERR_IO, LDB_ERR_SMALL_BUFFER
+ */
+LDB_RES ldb_upd_ind(LighDB *db, uint32_t index,
+		    void *data, uint32_t size);
+/**
+ * Add new item
+ *
+ * @param db pointer to DB structure
+ * @param data data of item
+ * @param size size of data
+ * @param id ID of new item
+ * @param newindex returns index of new item
+ * @return result LDB_OK, LDB_ERR_IO, LDB_ERR_SMALL_BUFFER
+ */
+LDB_RES ldb_add(LighDB *db,
+		void *data, uint32_t size,
+		uint32_t id, uint32_t *newindex);
+/**
+ * Get count of indexes of items with selected ID. And if list != 0 && len != 0 then put found indexes in the list
+ *
+ * @param db pointer to DB structure
+ * @param count returns count of found indexes or equals len
+ * @param list array with found indexes. Can be 0.
+ * @param len length of array. Can be 0. If len>0 and  (real count)<len then count will be equal to len and will be returned LDB_OK_SMALL_BUFFER.
+ * @return result LDB_OK, LDB_ERR_IO, LDB_OK_SMALL_BUFFER
+ */
+LDB_RES ldb_find_by_id(LighDB *db, uint32_t *count,
+		       uint32_t *list, uint32_t len);
 
 #endif
+
+
+
+
+
+
--- lighdb/lighdb_conf.h
+++ lighdb/lighdb_conf.h
@@ -5,4 +5,22 @@
 #define LIGHDB_USE_STDIO 1
 #define LDB_FILE
 
+#define LDB_MUTEX 0
+
+#if LDB_MUTEX == 1
+#include "FreeRTOS.h"
+#include "semphr.h"
+#define LDB_MUTEX_t xSemaphoreHandle //change for your OS
+//implement that functions for your OS
+
+//create mutex object
+uint8_t ldb_mutex_create (LDB_MUTEX_t *sobj);
+//delete mutex
+uint8_t ldb_mutex_delete (LDB_MUTEX_t *sobj);
+//Request Grant to Access some object
+uint8_t ldb_mutex_request_grant (LDB_MUTEX_t *sobj);
+//Release Grant to Access the Volume
+uint8_t ldb_mutex_release_grant (LDB_MUTEX_t *sobj);
+#endif
+
 #endif
